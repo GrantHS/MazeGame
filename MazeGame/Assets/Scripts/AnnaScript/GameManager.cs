4da5166b64ee58395b5d3d0f115840d8778c0250
extremeLworld <114627835+extremeLworld@users.noreply.github.com>
2023-09-12 12:16:02 -0700

    Added UI Blocker Script
    The UI blocker script is there so that the player can't click behind it.

--- MazeGame/Assets/Scripts/AnnaScript/GameManager.cs
+++ MazeGame/Assets/Scripts/AnnaScript/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
     private bool isPaused;
-    public GameObject pauseMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject optionsMenu;
     [SerializeField] private InputControls controls;
 
 
@@ -15,6 +18,7 @@ public class GameManager : MonoBehaviour
     {
         controls = new InputControls();
         pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
     }
 
 
@@ -22,12 +26,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.Escape))
         {
             pauseMenu.SetActive(true);
             PauseDaGame();
         }
-        if (Input.GetButtonDown("Pause"))
+        if (controls.Player1.Pause.triggered)
         {
             pauseMenu.SetActive(true);
             PauseDaGame();
@@ -43,8 +48,25 @@ public class GameManager : MonoBehaviour
         controls.Disable();
     }
 
+    
+
     public void PauseDaGame()
     {
-        
+        //i have zero fuckin idea why this keeps running in update even though it doesn't satisfy the if statement
+        Debug.Log("Ideally, when this function runs, it should freeze the scene and open the pause menu and also not constantly run in update.");
+    }
+
+    public void UnpauseDaGame()
+    {
+        pauseMenu.SetActive(false);
+    }
+    
+    public void OpenOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+    }
+    public void CloseOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
     }
 }
