6134512cb7ba1ebbe38f5da406d96637adde752f
GrantHS <6nohx1@gmail.com>
2023-09-15 17:15:03 -0700

    Player Respawn and Power Ups

---
+++ MazeGame/Assets/Scripts/GrantsScripts/PowerUp.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public ItemCollectables power;
+    private Rigidbody rb;
+    private float _rollSpeed = 5;
+
+    private void OnEnable()
+    {
+        Array values = Enum.GetValues(typeof(ItemCollectables));
+        System.Random random = new System.Random();
+        power = (ItemCollectables)values.GetValue(random.Next(values.Length));
+        Debug.Log("Item power: " +  power);
+
+        switch (power)
+        {
+            case ItemCollectables.Speed:
+                this.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                break;
+            case ItemCollectables.Strength:
+                this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                break;
+            case ItemCollectables.Invisibility:
+                this.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = UnityEngine.Random.onUnitSphere * _rollSpeed;
+    }
+}
