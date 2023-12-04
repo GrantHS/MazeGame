e49e651b69572143fe2bc11d901569f20dd10248
extremeLworld <114627835+extremeLworld@users.noreply.github.com>
2023-12-03 19:36:31 -0800

    UI changes
    - Added the GIF to the victory screen
    - Added the remaining credits images
    - Added those exit room textures.

--- MazeGame/Assets/Scripts/AnnaScript/VictoryScreen.cs
+++ MazeGame/Assets/Scripts/AnnaScript/VictoryScreen.cs
@@ -6,11 +6,13 @@ using TMPro;
 public class VictoryScreen : MenuParent
 {
     [SerializeField] private TextMeshProUGUI timeText;
-
-
+    [SerializeField] private GameObject dancingGif;
     private void OnEnable()
     {
         DisplayTime();
+
+        dancingGif.GetComponent<Animator>().Play("Dancing", 0);
+
         Time.timeScale = 0;
         GameManager.Instance.currentMenuOpened = UIMenu.Victory;
     }
